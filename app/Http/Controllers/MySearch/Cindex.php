46ffcca11847dd4f67f615c412167ba472537916
su.hanyu <su.hanyu@zol.com.cn>
2017-06-26 17:08:13 +0800

    first commit

---
+++ app/Http/Controllers/MySearch/Cindex.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\MySearch;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Cindex extends Controller
+{
+    /**
+     * @desc: 入口方法
+     * @author:Samuel Su(suhanyu)
+     * @date:17/6/26
+     * @param String $param
+     * @return Array
+     */
+    public function index() {
+        return [
+            'nishishei'
+        ];
+    }
+}
