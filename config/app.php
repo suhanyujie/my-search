520e7049da71fcf1e0073c5479735639e47f4b3c
suhanyujie <suhanyujie@qq.com>
2017-10-11 19:23:02 +0800

    1.新增laravoole的服务注册 2.调整项目中的时区

--- config/app.php
+++ config/app.php
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Shanghai', // UTC
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Laravoole\LaravooleServiceProvider::class,
 
     ],
 
